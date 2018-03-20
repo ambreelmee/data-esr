76c4b0f23a3224acf9d011391966636356742418
ambreelmee <ambreelmee@gmail.com>
2018-03-20 11:10:27 +0100

    add data fetching example

--- src/containers/Full/Full.js
+++ src/containers/Full/Full.js
@@ -3,8 +3,6 @@ import {Link, Switch, Route, Redirect} from 'react-router-dom';
 import {Container} from 'reactstrap';
 import Header from '../../components/Header/';
 import Sidebar from '../../components/Sidebar/';
-import Breadcrumb from '../../components/Breadcrumb/';
-import Aside from '../../components/Aside/';
 import Footer from '../../components/Footer/';
 
 import Update from '../../views/Update/';
--- src/views/Update/Update.js
+++ src/views/Update/Update.js
@@ -2,9 +2,38 @@ import React, { Component } from 'react';
 // import NewEntity from './NewEntity.js'
 import {Row, Col, Card, CardHeader, CardBody, Alert} from 'reactstrap';
 
+
 class Update extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hits: [],
+      isLoading: false,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true })
+    fetch('https://hn.algolia.com/api/v1/search?query=')
+    .then(response => response.json())
+    .then( data => {
+      this.setState({
+        hits: data.hits,
+        isLoading: false
+      })
+    })
+    .catch(error => this.setState({ error, isLoading: false }));
+  }
 
   render() {
+    const { hits, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
     return (
       <div className="animated fadeIn">
         <Row>
@@ -12,7 +41,13 @@ class Update extends Component {
             <Card>
               <CardBody>
                 <Alert color="info">
-                  Pas de mise à jour en attente
+                  <div>
+                    {hits.map(hit =>
+                      <div key={hit.objectID}>
+                        <a href={hit.url}>{hit.title}</a>
+                      </div>
+                    )}
+                  </div>
                 </Alert>
               </CardBody>
             </Card>
